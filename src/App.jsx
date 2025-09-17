1cea7c8e36c9636cee0ffd3f6ec5aa9d67f49690
Diane Foucher <152055207+Das-F@users.noreply.github.com>
2025-09-17 17:54:17 +0200

    charts components added

--- src/App.jsx
+++ src/App.jsx
@@ -2,79 +2,10 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
-
-const data = [
-  {
-    name: "Page A",
-    Kcal: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    Kcal: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    Kcal: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    Kcal: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    Kcal: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    Kcal: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    Kcal: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
-const Example = () => {
-  return (
-    <div style={{ width: "100%", height: 320 }}>
-      <ResponsiveContainer width="100%" height="100%">
-        <BarChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="pv" fill="#8884D8" />
-          <Bar dataKey="Kcal" fill="#82CA9D" />
-        </BarChart>
-      </ResponsiveContainer>
-    </div>
-  );
-};
+import BarChartComponent from "./components/Barchart.jsx";
+import RadarChartComponent from "./components/Radarchart.jsx";
+import LineChartComponent from "./components/LineChart.jsx";
+import PieChartComponent from "./components/PieChart.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -107,7 +38,10 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <Example />
+      <BarChartComponent />
+      <LineChartComponent />
+      <PieChartComponent />
+      <RadarChartComponent />
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
---
+++ src/components/BarChart.jsx
@@ -0,0 +1,77 @@
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
+
+const data = [
+  {
+    name: "Page A",
+    Kcal: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Page B",
+    Kcal: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Page C",
+    Kcal: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Page D",
+    Kcal: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "Page E",
+    Kcal: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Page F",
+    Kcal: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "Page G",
+    Kcal: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+const BarChartComponent = ({ data: propData }) => {
+  const chartData = propData && Array.isArray(propData) && propData.length ? propData : data;
+
+  return (
+    <div style={{ width: "100%", height: 320 }}>
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart
+          width={500}
+          height={300}
+          data={chartData}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="pv" fill="#8884D8" />
+          <Bar dataKey="Kcal" fill="#82CA9D" />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default BarChartComponent;
