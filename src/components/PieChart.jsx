ca57cc2e235fb46310dfdf046be0fd1ad79ff980
Diane Foucher <152055207+Das-F@users.noreply.github.com>
2025-09-19 16:48:16 +0200

    pieChart corrected

--- src/components/PieChart.jsx
+++ src/components/PieChart.jsx
@@ -1,63 +1,40 @@
-import { Pie, PieChart } from "recharts";
+import "./PieChart.css";
+import { Cell, Pie, PieChart, ResponsiveContainer } from "recharts";
 
-const data01 = [
-  {
-    name: "Group A",
-    value: 400,
-  },
-  {
-    name: "Group B",
-    value: 300,
-  },
-  {
-    name: "Group C",
-    value: 300,
-  },
-  {
-    name: "Group D",
-    value: 200,
-  },
-  {
-    name: "Group E",
-    value: 278,
-  },
-  {
-    name: "Group F",
-    value: 189,
-  },
-];
-const data02 = [
-  {
-    name: "Group A",
-    value: 2400,
-  },
-  {
-    name: "Group B",
-    value: 4567,
-  },
-  {
-    name: "Group C",
-    value: 1398,
-  },
-  {
-    name: "Group D",
-    value: 9800,
-  },
-  {
-    name: "Group E",
-    value: 3908,
-  },
-  {
-    name: "Group F",
-    value: 4800,
-  },
-];
+const data = [{ name: "Group A", value: 12 }];
+const color = "#FF0000";
 
 function PieChartComponent() {
   return (
-    <PieChart width={730} height={250}>
-      <Pie data={data02} dataKey="value" nameKey="name" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#FF0000" label />
-    </PieChart>
+    <div className="pie-chart-container" style={{ width: 280, height: 280, position: "relative" }}>
+      <h3
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          margin: 0,
+          fontSize: "1.5rem",
+          fontWeight: "normal",
+          color: "#74798C",
+          pointerEvents: "none",
+          textAlign: "center",
+          maxWidth: "120px",
+        }}
+      >
+        de votre objectif
+      </h3>
+
+      <ResponsiveContainer>
+        <PieChart>
+          <Pie data={data} cx="50%" cy="50%" innerRadius={85} outerRadius={100} cornerRadius={10} fill={color} dataKey="value">
+            {data.map((entry) => (
+              <Cell key={`cell-${entry.name}`} fill={color} />
+            ))}
+          </Pie>
+        </PieChart>
+      </ResponsiveContainer>
+    </div>
   );
 }
 
