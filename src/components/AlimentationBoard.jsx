cbddd9ef2a85607461ddc97dc68b6f29c320c4a8
Diane Foucher <152055207+Das-F@users.noreply.github.com>
2025-09-25 18:18:23 +0200

    alimentationboard data added

--- src/components/AlimentationBoard.jsx
+++ src/components/AlimentationBoard.jsx
@@ -1,20 +1,38 @@
 import "./AlimentationBoard.css";
 import AlimentationSymbol from "./AlimentationSymbol";
 
+const data = {
+  id: 18,
+  userInfos: {
+    firstName: "Cecilia",
+    lastName: "Ratorez",
+    age: 34,
+  },
+  score: 0.3,
+  keyData: {
+    calorieCount: 2500,
+    proteinCount: 90,
+    carbohydrateCount: 150,
+    lipidCount: 120,
+  },
+};
+
 function AlimentationBoard() {
+  const keyData = data.keyData || {};
+
   return (
     <div className="alimentation-board">
       <div className="alimentation-symbol-card">
-        <AlimentationSymbol img="/assets/AlimentationBoard-icons/Calories-icon.svg" value={120} unit="kcal" title="Calories" />
+        <AlimentationSymbol img="/assets/AlimentationBoard-icons/Calories-icon.svg" value={keyData.calorieCount} unit="kcal" title="Calories" />
       </div>
       <div className="alimentation-symbol-card">
-        <AlimentationSymbol img="/assets/AlimentationBoard-icons/Proteines-icon.svg" value={30} unit="g" title="Protéines" />
+        <AlimentationSymbol img="/assets/AlimentationBoard-icons/Proteines-icon.svg" value={keyData.proteinCount} unit="g" title="Protéines" />
       </div>
       <div className="alimentation-symbol-card">
-        <AlimentationSymbol img="/assets/AlimentationBoard-icons/Glucides-icon.svg" value={50} unit="g" title="Glucides" />
+        <AlimentationSymbol img="/assets/AlimentationBoard-icons/Glucides-icon.svg" value={keyData.carbohydrateCount} unit="g" title="Glucides" />
       </div>
       <div className="alimentation-symbol-card">
-        <AlimentationSymbol img="/assets/AlimentationBoard-icons/Lipides-icon.svg" value={20} unit="g" title="Lipides" />
+        <AlimentationSymbol img="/assets/AlimentationBoard-icons/Lipides-icon.svg" value={keyData.lipidCount} unit="g" title="Lipides" />
       </div>
     </div>
   );
