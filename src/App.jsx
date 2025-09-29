b9c1d42ceba766feb63bd7ee3e8b9025c3a4b460
Diane Foucher <152055207+Das-F@users.noreply.github.com>
2025-09-29 10:12:43 +0200

    API fetch added

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,6 @@ import PieChartComponent from "./components/PieChart.jsx";
 import AlimentationBoard from "./components/AlimentationBoard.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
-  // debug: vérifier que le composant se rend
-  console.log("App rendered, count =", count);
   // Afficher les infos de l'utilisateur 12 dans la console http://localhost:3000/user/12/activity
   function printUserActivity() {
     fetch("http://localhost:3000/user/12/activity")
